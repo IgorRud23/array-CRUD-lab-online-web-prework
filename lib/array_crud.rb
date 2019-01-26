cf18248812ab91fb56caae70c8d7cbb1d2789e72
Igor Rud <rud.igor23@gmail.com>
2019-01-26 22:30:19 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,7 +17,7 @@ array.unshift("#{element}")
 end
 
 def remove_element_from_end_of_array(array)
-  array = ["dog", "road", "car", "arrays"]
+  array = ["dog", "road", "car", "arrays!"]
   array.pop
 end
 
