9d54afd9b425f68730d0607760fbace28f446a23
Igor Rud <rud.igor23@gmail.com>
2019-01-26 22:36:57 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -28,8 +28,8 @@ end
 
 def retrieve_element_from_index(array, index_number)
 array = ["wow", "I", "am", "really", "learning", "arrays!"]
-index_number = "1"
-array["1"]
+index_number = [2]
+array+index_number
 end
 
 def retrieve_first_element_from_array(array)
