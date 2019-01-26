f596bcf7a0cf03b62d7623539b7e230a457b80c0
Igor Rud <rud.igor23@gmail.com>
2019-01-26 22:39:09 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -26,9 +26,9 @@ array = ["wow", "I", "am", "really", "learning", "arrays!"]
 array.shift
 end
 
-def retrieve_element_from_index(array, 2)
+def retrieve_element_from_index(array, index_number = 2)
 array = ["wow", "I", "am", "really", "learning", "arrays!"]
-array[2]
+array["#{index_number}"]
 end
 
 def retrieve_first_element_from_array(array)
